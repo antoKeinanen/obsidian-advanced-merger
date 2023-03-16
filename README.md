e94645d2706aaf13130da4adf805b33e69d4359d
antoKeinanen <anto.keinanen@gmail.com>
2023-03-16 18:33:51 +0200

    Initial commit
    Changes to be committed:
    	new file:   .editorconfig
    	new file:   .eslintignore
    	new file:   .eslintrc
    	new file:   .gitignore
    	new file:   .npmrc
    	new file:   README.md
    	new file:   esbuild.config.mjs
    	new file:   main.ts
    	new file:   manifest.json
    	new file:   media/2023-03-16 18-23-14.gif
    	new file:   package-lock.json
    	new file:   package.json
    	new file:   styles.css
    	new file:   tsconfig.json
    	new file:   version-bump.mjs
    	new file:   versions.json

---
+++ README.md
@@ -0,0 +1,2 @@
+# Obsidian Advanced Merge
+Merge a folder of notes for easier export. This plugin is designed for [obsidian](https://obsidian.md/) app.
---
+++ main.ts
@@ -0,0 +1,51 @@
+import { Plugin, TFolder } from "obsidian";
+
+export default class AdvancedMerge extends Plugin {
+	onload() {
+		this.registerEvent(
+			this.app.workspace.on("file-menu", (menu, file) => {
+				const folderOrFile = this.app.vault.getAbstractFileByPath(
+					file.path
+				);
+				if (folderOrFile instanceof TFolder) {
+					const folder = file;
+					menu.addItem((item) => {
+						item.setTitle("Merge files")
+							.setIcon("git-merge")
+							.onClick(async () => {
+								const { vault } = this.app;
+
+								let destination = await vault.create(
+									`${folder.path}-merged.md`,
+									""
+								);
+
+								const files = await Promise.all(
+									vault
+										.getMarkdownFiles()
+										.filter(
+											(file) =>
+												file.parent.path == folder.path
+										)
+										.reverse()
+								);
+
+								files.forEach(async (file) => {
+									let contents = await vault.read(file);
+									contents = `\n# ${file.name.replace(
+										/\.md$/,
+										""
+									)}\n${contents}\n`;
+									vault.append(destination, contents);
+								});
+							});
+					});
+				}
+			})
+		);
+	}
+
+	onunload() {
+		this.app.workspace.off("file-menu", () => {});
+	}
+}
